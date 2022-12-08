dc466d0eafeea626d0c41441f09028b3f992bf31
Tolaniee <Omotolaniolatunji135@gmail.com>
2022-12-08 13:55:26 -0800

    task 6 error fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main -Print out sizes of data type in C
- * code by Tolaniee
+ *
  * Return: 0
 */
+
 int main(void)
 {
 	char a;
@@ -11,10 +12,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
+
 	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-        printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
